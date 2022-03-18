e035f970ad4684efebfc3266f224d63cfa54049f
Atsukoro1 <dornicakkuba@gmail.com>
2022-03-18 19:19:44 +0100

    Settings menu

--- src/components/SettingsModal.tsx
+++ src/components/SettingsModal.tsx
@@ -1,15 +1,30 @@
+// React dependencies
 import React, { useState, forwardRef , useImperativeHandle} from "react";
 
+// MUI library components
 import Dialog from "@mui/material/Dialog";
 import DialogTitle from "@mui/material/DialogTitle";
 import DialogContent from "@mui/material/DialogContent";
 import DialogActions from "@mui/material/DialogActions";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
+import SnackBar from "@mui/material/Snackbar";
+import Alert from "@mui/material/Alert";
+
+// Other libraries
+import axios from "axios";
+
+// Create a new Axios instance
+const instance = axios.create({
+    baseURL: process.env.REACT_APP_BASE_URL,
+    withCredentials: true,
+});
 
 const SettingsModal = forwardRef((props : any, ref : any) => {
     const [opened, setOpened] = useState<boolean>(false);
+    const [snackbarState, setSnackbarState] = useState<any>({ opened: false, message: "", severity: "error" });
 
+    // Expose close function to parent component
     useImperativeHandle(ref, () => ({
         open: () => {
             setOpened(true);
@@ -20,37 +35,96 @@ const SettingsModal = forwardRef((props : any, ref : any) => {
         }
     }));
 
-    const onChangeSettings = async (e : React.SyntheticEvent) : Promise<void> => {
+    const onClose = () : void => {
+        ref.current.close();
+    }
+
+    const hideSnackBar = () : void => {
+        return setSnackbarState({ message: "", severity: "error", opened: false });
+    }
+
+    // Will be emitted when user submits the settings form
+    const onFormSubmit = async (e : React.SyntheticEvent) : Promise<void> => {
         e.preventDefault();
 
         const target = e.target as typeof e.target & {
-            newEmail: { value: string };
-            newPassword: { value: string };
-            newUsername: { value: string };
-            password: { value: string };
+          newUsername: { value?: string };
+          newEmail: { value?: string };
+          newPassword: { value?: string };
+          password: { value: string };
         };
 
-        console.log(target.newUsername)
+        const formData = {
+            newEmail: target.newEmail.value,
+            newUsername: target.newUsername.value,
+            newPassword: target.newPassword.value,
+            password: target.password.value
+        };
+
+        const response = await instance({
+            url: `${process.env.REACT_APP_BASE_URL}/api/user/settings/change`,
+            method: "PATCH",
+            data: formData
+        });
+
+        if(!response.data.success) return setSnackbarState({ opened: true, message: response.data.errors, severity: "error" });
+
+        onClose();
+        return setSnackbarState({ opened: true, message: "Successfully changed settings", severity: "success" });
     };
 
     return (
-        <form onSubmit={onChangeSettings}>
-            <Dialog onClose={() => { ref.current.close(); }} open={opened}>
+        <>
+        <Dialog onClose={onClose} open={opened}>
+            <form onSubmit={onFormSubmit}>
                 <DialogTitle>Settings</DialogTitle>
 
                 <DialogContent>
-                    <TextField type="text" name="newUsername" variant="outlined" required sx={{ marginTop: 1, marginBottom: 1.5 }} label="Email"></TextField><br/>
-                    <TextField type="email" name="newEmail" variant="outlined" required sx={{ marginBottom: 1.5 }} label="Password"></TextField><br/>
-                    <TextField type="password" name="newPassword" variant="outlined" required sx={{ marginBottom: 1.5 }} label="New password"></TextField><br/>
-                    <TextField type="password" name="password" variant="outlined" required label="Password"></TextField>
+                        <TextField 
+                            label="New username" 
+                            name="newUsername" 
+                            variant="outlined"
+                            sx={{ marginBottom: 1.5, marginTop: 1 }}
+                            type="text"/>
+                            
+                        <br/>
+                        <TextField 
+                            label="New email" 
+                            name="newEmail" 
+                            variant="outlined"
+                            sx={{ marginBottom: 1.5 }}
+                            type="email"/>
+
+                        <br/>
+                        <TextField 
+                            label="New password" 
+                            name="newPassword" 
+                            variant="outlined"
+                            sx={{ marginBottom: 1.5 }}
+                            type="password"/>
+
+                        <br/>
+                        <TextField 
+                            required
+                            label="Password" 
+                            name="password" 
+                            variant="outlined"
+                            type="password"/>
                 </DialogContent>
-    
+
                 <DialogActions>
-                    <Button onClick={() => { ref.current.close() }}>Close</Button>
-                    <Button type="submit" variant="contained" onClick={onChangeSettings}>Save</Button>
+                    <Button onClick={onClose} variant="outlined">Close</Button>
+                    <Button type="submit" variant="contained">Save</Button>
                 </DialogActions>
-            </Dialog>
-        </form>
+            </form>
+        </Dialog>
+
+        <SnackBar onClose={hideSnackBar} autoHideDuration={3000} open={snackbarState.opened}>
+            <Alert severity={snackbarState.severity}>
+                {snackbarState.message}
+            </Alert>
+        </SnackBar>
+        </>
       )
 });
 
